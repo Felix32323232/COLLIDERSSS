8b531a5f67c18485a88506056d48e7f5b65e3c24
Felix32323232 <felixramirochulvi2@gmail.com>
2026-04-29 17:51:55 +0200

    FINAL

--- Assets/BallLauncher.cs
+++ Assets/BallLauncher.cs
@@ -5,38 +5,69 @@ using UnityEngine.InputSystem.XR.Haptics;
 public class BallLauncher : MonoBehaviour
 {
     Rigidbody rb;
-    public Vector3 direction;
-    public Vector3 rotation;
     public GameObject direccion;
     public PlayerInput playerInput;
-    public float velDesplazamiento = 4.3f;
-    
+    public float velDesplazamiento;
+    public int fuerzaLanzamiento;
 
+    bool hasLaunched = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
-;
-
     }
 
     void Update()
     {
         Vector2 movement = playerInput.actions["Move"].ReadValue<Vector2>();
-        direction = direccion.transform.position;
-        transform.eulerAngles +=  new Vector3(0, movement.x * velDesplazamiento, 0) * Time.deltaTime;
+        transform.eulerAngles += new Vector3(0, movement.x * velDesplazamiento, 0) * Time.deltaTime;
 
+        if (rb.linearVelocity.sqrMagnitude <= 0.0001)
+        {
+            direccion.SetActive(true);
+            hasLaunched = false;
+        }
+    }       
 
-        //(movement.y * velDesplazamiento, movement.x * velDesplazamiento, 0); 
-
-
-    }
-    
     public void Launch()
     {
-        rb.AddForce(transform.forward);
-    }
-   
+        if (hasLaunched) return;
 
+        if (direccion != null)
+        {
+            direccion.SetActive(false);
+        }
+
+        if (rb != null)
+        {
+            rb.AddForce(transform.forward * fuerzaLanzamiento);
+        }
+
+
+
+        hasLaunched = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Deadzone"))
+        {
+            if (direccion != null)
+            {
+                direccion.SetActive(true);
+            }
+
+            if (rb != null)
+            {
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+
+            transform.position = new Vector3(0f, 0.35f, -4.4f);
+            transform.eulerAngles += new Vector3(0,0,0);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            hasLaunched = false;
+        }
+    }
 }
